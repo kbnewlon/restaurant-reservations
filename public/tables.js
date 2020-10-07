d79b63aa97ff6195295a412924e980eb084b83eb
Joshua Jainga <jjainga@gmail.com>
2020-10-07 13:48:34 -0700

    saved get request

--- public/tables.js
+++ public/tables.js
@@ -19,3 +19,32 @@ $.get("/api/reservations/", function(data) {
     reservationsDiv.append($("<br>"))
     }
 });
+
+$.get("/api/waitlist/", function(response) {
+    //console.log(response);
+    reservationsDiv.append($("<h3>").text("Waiting List"))
+        for(let i = 0; i < response.length; i++) {
+        //Create the divs
+            const container = $("<div style='border: solid 1px; border-radius: 5px;'>>");
+            let tableNumber = $("<h1>").addClass("tableNumber");
+            let hr = $("<hr>");
+            let id = $("<p>").addClass("id");
+            let name = $("<p>").addClass("name");
+            let email = $("<p>").addClass("email");
+            let phone = $("<p>").addClass("phone");
+        //setting values
+            let number = i + 1;
+            tableNumber.text(`Table #${number}`);
+            name.text(`Customer Name: ${response.customerName}`);
+            phone.text(`Phone Number: ${response.phoneNumber}`);
+            email.text(`Email: ${response.customerEmail}`);
+            id.text(`Customer ID: ${response.customerID}`);
+        //appending values to div
+            container.append(tableNumber);
+            container.append(hr);
+            container.append(name);
+            container.append(phone);
+            container.append(email);
+            container.append(id);
+            waitlistDiv.append(container);  
+    }});
